341fc951a69e737bd1620ece227f53846da2eb07
athiraanil11 <athiraanil093@gmail.com>
2022-12-10 14:02:37 +0530

    complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddnewsComponent } from './addnews/addnews.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ViewallnewsComponent } from './viewallnews/viewallnews.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+const myroutes=[
+
+  {path:"",component:AddnewsComponent},
+  {path:"view",component:ViewallnewsComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddnewsComponent,
+    NavbarComponent,
+    ViewallnewsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myroutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
